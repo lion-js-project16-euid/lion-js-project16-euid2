5d001922060b7945e64240ce0d41ad28999fcc8b
ingbinsee <ingbinsee@gmail.com>
2023-07-31 11:33:10 +0900

    ✨ 현재시간 렌더링 기능 구현

--- client/lib/dom/render.js
+++ client/lib/dom/render.js
@@ -1,12 +1,26 @@
 import { insertLast } from './insert.js';
 
+export function nowTime() {
+  let today = new Date();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  if (hours < 10) {
+    return '0' + hours + ':' + minutes;
+  } else {
+    if (minutes < 10) {
+      return hours + ':' + '0' + minutes;
+    }
+    return hours + ':' + minutes;
+  }
+}
+
 function createPost(item) {
-  const { id, name, price, location, upload, image, like } = item;
+  const { id, href, name, price, location, upload, image, like } = item;
   const { img, alt } = image;
 
   const templete = /* html */ `
   <article class="h-[130px] relative border-b border-[#D3D3D3] border-solid data-[index]:${id}">
-    <a href="">
+    <a href="${href}">
       <div class="flex flex-row-reverse p-3">
         <div class="flex flex-col grow px-6 py-3">
           <h2 class="text-sm">${name}</h2>
@@ -14,7 +28,7 @@ function createPost(item) {
             <strong class="text-sm">${price}</strong>
             <div class="flex items-center text-[10px] text-contents">
               <span>${location}</span>
-              <span class="p-1">&middot;</span>
+              <span aria-hidden="true" class="p-1">&middot;</span>
               <span>${upload}</span>
             </div>
           </div>
