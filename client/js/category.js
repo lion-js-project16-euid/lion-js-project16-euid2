9a8968ac9d53271ad287d0860f4725c9da900a3e
민규 <xiahtic73@gmail.com>
2023-07-31 23:42:41 +0900

    ✨ category.html 완성

--- client/js/category.js
+++ client/js/category.js
@@ -1,37 +1,57 @@
-import { getNode, toggleClass, attr, loadStorage, saveStorage, deleteStorage } from '/lib/index.js';
+import {
+  getNode,
+  getNodes,
+  toggleClass,
+  attr,
+  loadStorage,
+  saveStorage,
+  deleteStorage,
+  addClass,
+  removeClass,
+} from '/lib/index.js';
 
 const ul = getNode('.categoryList');
+const listNodes = getNodes('.categoryList li');
+const searchBox = getNode('.searchBox');
 let valueArray = [];
 
-function setChecked(e) {
+function handleCategoryClass(e) {
   const li = e.target.closest('li');
-  const checkbox = e.target.closest('input');
+  const isChecked = e.target.closest('input').checked;
   const checkImg = e.target.closest('div').querySelector('span');
-  const isChecked = checkbox.checked;
-  if (!li) return;
-
   const value = attr(e.target, 'id').slice(8);
 
-  if (!isChecked) {
-    toggleClass(li, 'bg-gray-400');
-    toggleClass(li, 'bg-primary');
-    toggleClass(li, 'text-white');
-    toggleClass(checkImg, 'bg-icon-plus');
-    toggleClass(checkImg, 'bg-icon-checked');
-    valueArray = valueArray.filter(i => i !== value);
-    console.log(valueArray);
-  } else {
-    toggleClass(li, 'bg-gray-400');
-    toggleClass(li, 'bg-primary');
-    toggleClass(li, 'text-white');
-    toggleClass(checkImg, 'bg-icon-plus');
-    toggleClass(checkImg, 'bg-icon-checked');
-    valueArray.push(value);
-    console.log(valueArray);
-  }
+  toggleClass(li, 'bg-gray-400');
+  toggleClass(li, 'bg-primary');
+  toggleClass(li, 'text-white');
+  toggleClass(checkImg, 'bg-icon-plus');
+  toggleClass(checkImg, 'bg-icon-checked');
 
+  isChecked ? valueArray.push(value) : (valueArray = valueArray.filter(i => i !== value));
   const category = valueArray.join(', ');
   loadStorage(`category`) !== '' ? saveStorage(`category`, category) : deleteStorage(`category`);
 }
 
-ul.addEventListener('change', setChecked);
+function setCategoryChecked(node) {
+  node.addEventListener('change', handleCategoryClass);
+}
+
+setCategoryChecked(ul);
+
+/* -------------------------------------------------------------------------- */
+
+function searchCategory(e) {
+  const text = e.target.value;
+
+  if (text === '') {
+    listNodes.forEach(li => removeClass(li, 'hidden'));
+  } else if (text) {
+    listNodes.forEach(li => {
+      let isIncludeText =
+        li.children[0].children[0].innerText.includes(text) || li.children[0].children[1].innerText.includes(text);
+      !isIncludeText ? addClass(li, 'hidden') : removeClass(li, 'hidden');
+    });
+  }
+}
+
+searchBox.addEventListener('input', searchCategory);
