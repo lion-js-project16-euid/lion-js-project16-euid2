2255dbad307a02b8197195b9ada5998e62bde754
ingbinsee <ingbinsee@gmail.com>
2023-08-01 04:57:14 +0900

    ➕ 기기 상세페이지 렌더링 기능 구현

---
+++ client/js/detail4_jooyonMonitor.js
@@ -0,0 +1,27 @@
+import { getNode, tiger, renderDetail, attr } from '../lib/index.js';
+
+const URL = 'http://localhost:3000/device/4';
+
+const postDetail = getNode('.detail-wrapper');
+const response = await tiger.get(URL);
+const data = response.data;
+
+const detailButton = getNode('.detail-button');
+
+async function renderDetailList() {
+  response;
+  renderDetail(postDetail, data);
+}
+
+let status = true;
+function handleLikeButton() {
+  if (status === true) {
+    attr('.detail-like-button', 'src', '/assets/icon-heart-strignt-full.svg');
+  } else {
+    attr('.detail-like-button', 'src', '/assets/icon-heart-strignt.svg');
+  }
+  status = !status;
+}
+
+renderDetailList();
+detailButton.addEventListener('click', handleLikeButton);
