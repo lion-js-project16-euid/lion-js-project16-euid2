f37ef0b062cce6378a634c05b6a426ca305c5b61
Imeanstar <mike4404@naver.com>
2023-08-02 00:15:39 +0900

    ✨index화면 접속 메커니즘 변화, login,sign-up 에러 수정 및 엔터수정

--- client/js/main.js
+++ client/js/main.js
@@ -12,10 +12,7 @@ temp.addEventListener('click', function (e) {
   const uniqueNum = valuesForUnique[indexOfUnique];
 
   console.log(indexOfCategory);
-  if (indexOfCategory == -1) {
-    alert('아직 category를 선택하지 않은 상태입니다. 다시 선택해주세요.');
-    window.location.href = '/views/category.html';
-  } else if (typeof uniqueNum !== 'undefined') {
+  if (typeof uniqueNum !== 'undefined') {
     console.log(1);
     if (uniqueNum.length != 10) {
       alert('해당 uniqueID는 로그아웃, 만료 등의 이슈로 유효하지 않습니다. 다시 로그인해주세요.');
@@ -23,8 +20,11 @@ temp.addEventListener('click', function (e) {
     } else {
       window.location.href = '/views/story.html';
     }
-  } else {
+  } else if (typeof uniqueNum == 'undefined') {
     console.log(2);
     window.location.href = '/views/sign-up.html';
+  } else if (indexOfCategory == -1) {
+    alert('아직 category를 선택하지 않은 상태입니다. 다시 선택해주세요.');
+    window.location.href = '/views/category.html';
   }
 });
