694fa8e613db85b93df5cd0dc4c9e82149389b91
Imeanstar <mike4404@naver.com>
2023-08-01 17:51:03 +0900

    ✨ 카테고리 미선택시 카테고리 선택하도록 수정, login disabled추가

--- client/js/main.js
+++ client/js/main.js
@@ -8,9 +8,14 @@ temp.addEventListener('click', function (e) {
   const keysForUnique = Object.keys(localStorage);
   const valuesForUnique = Object.values(localStorage);
   const indexOfUnique = keysForUnique.indexOf('uniqueID');
+  const indexOfCategory = keysForUnique.indexOf('category');
   const uniqueNum = valuesForUnique[indexOfUnique];
 
-  if (typeof uniqueNum !== 'undefined') {
+  console.log(indexOfCategory);
+  if (indexOfCategory == -1) {
+    alert('아직 category를 선택하지 않은 상태입니다. 다시 선택해주세요.');
+    window.location.href = '/views/category.html';
+  } else if (typeof uniqueNum !== 'undefined') {
     console.log(1);
     if (uniqueNum.length != 10) {
       alert('해당 uniqueID는 로그아웃, 만료 등의 이슈로 유효하지 않습니다. 다시 로그인해주세요.');
