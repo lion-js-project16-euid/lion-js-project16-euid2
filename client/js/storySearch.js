326f7fe95ae853f4c7da51ddabafaa82a2430c24
seobinbang7 <newmso5382@gmail.com>
2023-08-02 00:17:27 +0900

    ➕ storySearch.js search 기능이 녹여있는 파일 추가

---
+++ client/js/storySearch.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const searchForm = document.getElementById('searchForm');
+  const searchInput = searchForm.querySelector('input[name="searchQuery"]');
+  const postList = document.getElementById('postList');
+
+  searchForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const formData = new FormData(searchForm);
+    const searchQuery = formData.get('searchQuery');
+
+    searchInput.value = '';
+
+    try {
+      const response = await fetch('http://localhost:3000/searchData');
+      const posts = await response.json();
+
+      const filteredPosts = posts.filter(post =>
+        post.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+
+      displayPosts(filteredPosts);
+    
+    } catch (error) {
+      console.error('Error fetching product data:', error);
+      return [];
+    }
+  });
+});
+
+function displayPosts(posts) {
+  postList.innerHTML = '';
+  if (posts.length === 0) {
+    postList.innerHTML = '<p>검색 결과가 없습니다.</p>';
+    return;
+  }
+
+  const postItems = posts.map(post =>
+    `<article class="h-[130px] relative border-b border-[#D3D3D3] border-solid data-[index]:10">
+      <a href=${post.url}>
+        <div class="flex flex-row-reverse p-3">
+          <div class="flex flex-col grow px-6 py-3">
+            <h2 class="text-sm">${post.title}</h2>
+            <div class="flex flex-col-reverse">
+              <div class="state flex">    
+                <strong class="text-sm">${post.price}</strong>
+              </div>
+              <div class="flex items-center text-[10px] text-contents">
+                <span>${post.location}</span>
+                <span aria-hidden="true" class="p-1">&middot;</span>
+                <span>${post.upload}</span>
+              </div>
+            </div>
+          </div>
+            <img src=${post.imgSrc} alt=${post.alt} class="w-[140px] h-[110px] rounded-lg" />
+        </div>
+      </a>`
+  );
+  postList.innerHTML = postItems.join('');
+}
