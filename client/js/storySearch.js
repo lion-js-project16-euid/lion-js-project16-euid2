3f5da0fc21fc9311d4d2b5795ed2904c0d08a140
Imeanstar <mike4404@naver.com>
2023-08-02 09:15:20 +0900

    1

--- client/js/storySearch.js
+++ client/js/storySearch.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const searchInput = searchForm.querySelector('input[name="searchQuery"]');
   const postList = document.getElementById('postList');
 
-  searchForm.addEventListener('submit', async (event) => {
+  searchForm.addEventListener('submit', async event => {
     event.preventDefault();
     const formData = new FormData(searchForm);
     const searchQuery = formData.get('searchQuery');
@@ -14,12 +14,9 @@ document.addEventListener('DOMContentLoaded', () => {
       const response = await fetch('http://localhost:3000/searchData');
       const posts = await response.json();
 
-      const filteredPosts = posts.filter(post =>
-        post.title.toLowerCase().includes(searchQuery.toLowerCase())
-      );
+      const filteredPosts = posts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()));
 
       displayPosts(filteredPosts);
-    
     } catch (error) {
       console.error('Error fetching product data:', error);
       return [];
@@ -34,8 +31,9 @@ function displayPosts(posts) {
     return;
   }
 
-  const postItems = posts.map(post =>
-    `
+  const postItems = posts.map(
+    post =>
+      `
       <article class="h-[130px] relative border-b border-[#D3D3D3] border-solid data-[index]:10">
         <a href=${post.url}>
           <div class="flex flex-row-reverse p-3">
